c0c7f23679d03db4375d1fb184dea53987de149e
Rodrigo Pardo <r.pardo03@ufromail.cl>
2019-12-19 23:26:07 -0300

    Se agregan animaciones a las paginas html

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,7 @@
 import Control.Controlador;
-import Control.GestorRegistro;
-import Modelo.DatoJSON;
-import Modelo.Registro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @SpringBootApplication
